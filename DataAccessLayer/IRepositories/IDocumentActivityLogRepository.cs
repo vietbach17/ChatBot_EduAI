c1f3f8cdbd7baf47ac1c0601e715303ca52290b1
Nguyen Trung Nghia <nghia011104@gmail.com>
2026-07-08 12:54:42 +0700

    feat(doc-dal): Implement DocumentRepository and DocumentActivityLogRepository

---
+++ DataAccessLayer/IRepositories/IDocumentActivityLogRepository.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.IRepositories
+{
+    internal interface IDocumentActivityLogRepository
+    {
+        Task AddLogAsync(DocumentActivityLog log);
+        Task<IEnumerable<DocumentActivityLog>> GetLogsBySubjectIdAsync(int subjectId);
+    }
+}
